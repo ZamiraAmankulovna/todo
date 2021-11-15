1fc5bda722021e6f701f98587f07f35ac8527550
Zamira <akibonoaki@gmail.com>
2021-11-15 21:05:13 +0600

    added todo_list in task page

--- homework/views.py
+++ homework/views.py
@@ -1,11 +1,13 @@
 from django.shortcuts import render, HttpResponse
+from .models import ToDo
 
 # Create your views here.
 def homework(request):
     return render(request, "index.html")
 
 def task(request):
-    return render(request, 'task.html')
+    todo_list = ToDo.objects.all()
+    return render(request, 'task.html', {"todo_list": todo_list})
 
 def second(request):
     return HttpResponse('Test2 page')
